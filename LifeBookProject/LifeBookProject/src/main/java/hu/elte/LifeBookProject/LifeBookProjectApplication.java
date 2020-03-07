7bff01f5318554ad340620a7f66078c865431767
habzdafruzsina <56350697+habzdafruzsina@users.noreply.github.com>
2020-03-07 03:25:08 +0100

    Created Maven project\nCreated Maven project for a base, and added .gitignore for maven files and java files.

---
+++ LifeBookProject/LifeBookProject/src/main/java/hu/elte/LifeBookProject/LifeBookProjectApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.LifeBookProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeBookProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeBookProjectApplication.class, args);
+	}
+
+}
