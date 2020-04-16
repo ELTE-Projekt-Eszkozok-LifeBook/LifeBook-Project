cbbc429a4903a18bc965a37e53b1f513897ce028
konczbianka <konczbianka@gmail.com>
2020-04-16 17:13:24 +0200

    Modified script file

--- script.bat
+++ script.bat
@@ -1,2 +1,7 @@
+git clone https://github.com/ELTE-Projekt-Eszkozok-LifeBook/LifeBook-Project.git
+
+cd LifeBook-Project/LifeBookProject/LifeBookProject
 call mvn clean package
 call mvn spring-boot:run
+
+explorer http://localhost:8080/
