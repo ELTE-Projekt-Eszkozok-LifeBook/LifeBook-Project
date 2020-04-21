eccde4cc034127beacc21a43dd05a80406535fce
habzdafruzsina <56350697+habzdafruzsina@users.noreply.github.com>
2020-04-21 16:44:36 +0200

    Modified push-javadoc-to-gh-pages.sh and travis.yml 9x

--- push-javadoc-to-gh-pages.sh
+++ push-javadoc-to-gh-pages.sh
@@ -4,7 +4,7 @@ if [ "$TRAVIS_REPO_SLUG" == "ELTE-Projekt-Eszkozok-LifeBook/LifeBook-Project" ] 
 
   echo -e "Publishing javadoc...\n"
 
-  cp -R build/docs/javadoc $HOME/javadoc-latest
+  cp -R LifeBookProject/LifeBookProject/target/site/apidocs $HOME/javadoc-latest
 
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
